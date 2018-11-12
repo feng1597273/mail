edf45e88bdc694310a1eda0a010e25d8ca711204
feng <feng7273@qq.com>
2018-11-13 01:13:12 +0800

    添加less和图标

---
+++ src/view/Home.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class='home'>
+		hello
+	</div>
+</template>
+
+<script>
+export default{
+	name:"home",
+	data(){
+		return{
+
+		}
+	},
+	mounted(){
+
+	},
+	methods:{
+
+	}
+}
+</script>
+
+<style lang='less' scoped>
+@import '~@/assets/styles/variables.less';
+.home{
+	background:@bgColor;
+}
+</style>
